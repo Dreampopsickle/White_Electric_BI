b86c1e43f51e71395d6331e8b00f4b64de982025
Christian <CRakotoarisoa@gmail.com>
2025-01-25 18:52:07 -0500

    refactor dbt models and schema yaml again

--- target/compiled/square_pipeline_project/square_pipeline_project/models/staging/stg_orders.sql
+++ target/compiled/square_pipeline_project/square_pipeline_project/models/staging/stg_orders.sql
@@ -4,9 +4,9 @@ SELECT
     TRIM(order_id) as order_id,
     created_at AS order_datetime,
     TRIM(state) AS order_state,
-    CAST(total_money AS FLOAT64) / 100 AS total_order_amount,
-    CAST(total_tax_money AS FLOAT64) / 100 AS total_tax,
-    CAST(total_tip_money AS FLOAT64) / 100 AS total_tip,
-    CAST(total_discount_money AS FLOAT64) / 100 AS total_discount
+    total_money  AS total_order_amount,
+    total_tax_money AS total_tax,
+    total_tip_money AS total_tip,
+    total_discount_money AS total_discount
 FROM 
     `we-analysis`.`Sales_Data`.`orders_data`
